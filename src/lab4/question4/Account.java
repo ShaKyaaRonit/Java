4e5c7560bd90fe0346b742a36cf8b6725864bd0e
ShaKyaaRonit <shkronit@gmail.com>
2024-11-18 09:14:16 -0800

    shifted completed lab4

---
+++ src/lab4/question4/Account.java
@@ -0,0 +1,19 @@
+package lab4.question4;
+public class Account {
+    void deposit(){
+     System.out.println("Debited");
+    }
+    void withdrawn(){
+     System.out.println("Credited");
+    }
+    void viewbalance(){
+     System.out.println("Remaining Balance");
+    }
+  public static void main(String[] args) {
+    CurrentAccount RonitCurrent = new CurrentAccount(2000,1);
+    SavingAccount RonitSaving = new SavingAccount(1000000, 10);
+    RonitCurrent.dislayingcurrentAccount();
+    RonitSaving.display();
+
+  }
+}
