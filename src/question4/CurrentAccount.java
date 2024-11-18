87ca3154077291cace31638da3f6b0767a3cf52d
ShaKyaaRonit <shkronit@gmail.com>
2024-11-18 08:35:40 -0800

    question4

---
+++ src/question4/CurrentAccount.java
@@ -0,0 +1,13 @@
+package question4;
+public class CurrentAccount {
+    double balance;
+    int overdraftLimit;
+
+    CurrentAccount(double b,int o){
+        this.balance= b;
+        this.overdraftLimit=o;
+    }
+    void dislayingcurrentAccount(){
+        System.out.println("balance:"+ balance + "overdraftLimit:"+ overdraftLimit);
+    }
+}
---
+++ src/question4/SavingAccount.java
@@ -0,0 +1,16 @@
+package question4;
+
+public class SavingAccount {
+ double balance;
+ double interestRate;
+    SavingAccount(double balance,double interestRate)
+    {
+    this.balance=balance;
+    this.interestRate=interestRate;
+    }
+    void display()
+    {
+        System.out.println("balance:"+ balance + "interest rate:"+interestRate);
+    }
+}
+
