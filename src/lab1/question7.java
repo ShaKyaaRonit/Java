5c83ca8f95baf43571763ba427c0d519454051e2
ShaKyaaRonit <shkronit@gmail.com>
2024-11-21 20:38:18 +0545

    question7

---
+++ src/lab1/question7.java
@@ -0,0 +1,23 @@
+//Write a program to find the greatest common divisor of two numbers.
+package lab1;
+import java.util.Scanner;
+public class question7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+        int gcd = findGCD(num1, num2);
+        System.out.println("The GCD of " + num1 + " and " + num2 + " is: " + gcd);
+        scanner.close();
+    }
+    private static int findGCD(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
