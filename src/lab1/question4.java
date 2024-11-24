14e55a37188fdf7552fe14fab6769316d882242d
ShaKyaaRonit <shkronit@gmail.com>
2024-11-24 06:04:10 +0545

    using only if else

--- src/lab1/question4.java
+++ src/lab1/question4.java
@@ -1,21 +1,38 @@
 package lab1;    
 import java.util.Scanner;
 public class question4 {
-    public static void main(String[] args) {
-        int a;
-        int b; 
-        Scanner acv = new Scanner(System.in);
-        System.out.println("Enter three numbers:");
-        a = acv.nextInt(); 
-        b = acv.nextInt();
-        a = (a > b) ? a : b;  // storing greater variable in inital variable a 
-
-        b = acv.nextInt(); // thid input taking in b to check greatest value which is a and replace greaterst in a again.
-        a = (b > a) ? b : a;   
-
-        System.out.println("The greatest number is: " + a);
-        acv.close();
+        static int  a;
+        static int  b;
+        public static void main(String[] args) {
+            Scanner avb= new Scanner(System.in);
+            System.out.println("enter num");
+            a= avb.nextInt();
+            System.out.println("enter num");
+            b= avb.nextInt();
+            if (a<b)
+            System.out.println("greater"+b);
+            else{
+                System.out.println("greater"+a);
+            }
+    
+    if (a<b ){
+            //third input
+            System.out.println("third integer as well:");
+            a = avb.nextInt();
     }
-
-}
+    else {
+            //third input
+            System.out.println("third integer as well:");
+            b = avb.nextInt();
+    } 
+    if (a<b)
+    System.out.println("greater"+b);
+    else{
+        System.out.println("greater"+a);
+    }
+    
+            avb.close();
+        }
+    }
+    
 
